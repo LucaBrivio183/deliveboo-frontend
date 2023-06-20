697184325740d38ba9d7468ec126652a1e00e7d4
Luca Brivio <118833667+LucaBrivio183@users.noreply.github.com>
2023-06-20 12:10:23 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+//libraries
+import axios from 'axios';
+//function
+import { store } from './store';
+//components
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  created() {
+
+  }
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped>
+@use './assets/scss/main.scss' as *;
+</style>
