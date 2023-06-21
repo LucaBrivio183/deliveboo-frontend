6513353cf28acbdc7126284045ba3d7d1eea9de4
vittoriaromano <vittoria5005@gmail.com>
2023-06-21 18:15:09 +0400

    feat: axios call fix

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+    apiBaseUrl: 'http://127.0.0.1:8000/api',
+    apiUrls: {
+        homepage: '/homepage',
+        typologies:'/typologies'
+        }
 
 })
