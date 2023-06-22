34037f0742cac76c887bf48e78e05e1cb0685fa7
LucaBrivio183 <l.brivio4@gmail.com>
2023-06-22 17:17:55 +0200

    feat: filtered api call

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -13,7 +13,8 @@ export default {
             store,
             typologies: [],
         }
-    },methods:{
+    },
+    methods:{
         getTypologies() {
             axios.get('http://127.0.0.1:8000/api/homepage/typologies')
                 .then((response) => {
@@ -29,7 +30,7 @@ export default {
 </script>
 
 <template>
-        <div class="px-0 bg-dark">
+        <aside class="px-0 bg-dark">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                 <a href="#" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <span class="fs-5 d-none d-sm-inline">Preferenze?</span>
@@ -38,14 +39,14 @@ export default {
                     <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                         <li v-for="typology in typologies" class="nav-item text-white mb-3">
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" :value="typology.id" id="typology">
-                                <label class="form-check-label" for="typology">{{ typology.name}}</label>
+                                <input class="form-check-input" type="checkbox" :value="typology.id" :id="typology.name" v-model="store.selectedTypologies" @change="$emit('getRestaurants')">
+                                <label class="form-check-label" :for="typology.name" :name="typology.name">{{ typology.name}}</label>
                             </div>
                         </li>
                     </ul>
                 </form>
             </div>
-        </div> 
+        </aside> 
 </template>
 
 <style lang="scss" scoped>
