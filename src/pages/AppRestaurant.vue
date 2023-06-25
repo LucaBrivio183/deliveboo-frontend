02adc6d8938bbbc8ef689b4bfb250220ca4d5847
eugeniarossi <eugenia.rossi@outlook.com>
2023-06-25 19:14:13 +0200

    feat: restaurant details

--- src/pages/AppRestaurant.vue
+++ src/pages/AppRestaurant.vue
@@ -18,7 +18,7 @@ export default {
     },
     methods: {
         // Axios call to get a specified restaurant with its products
-        getProducts() {  
+        getProducts() {
             axios.get(`${this.store.apiBaseUrl}${this.store.apiUrls.restaurant}/${this.$route.params.slug}`)       //Todo: replace "burger-king" with /${this.$route.params.slug}
                 .then((response) => {
                     this.restaurant = response.data.results;
@@ -33,19 +33,34 @@ export default {
 </script>
 
 <template>
+    <!-- restaurant image -->
+    <div class="restaurant-image">
+        <img src="https://picsum.photos/900/500" alt="restaurant" class="w-100">
+    </div>
+    <!-- /restaurant image -->
+    <!-- restaurant details -->
+    <div id="restaurant-details" class="p-4" style="width: 27.5rem;">
+        <div class="card-body d-flex flex-column align-items-center">
+            <h2 class="mt-2 mb-3 fw-bold">{{ restaurant.name }}</h2>
+            <p class="my-1">{{ restaurant.address }}, {{ restaurant.city }}, {{ restaurant.postal_code }}.</p>
+            <p class="my-1">Orari di apertura: {{ restaurant.business_times }}</p>
+            <p class="my-1">Tel: {{ restaurant.phone_number }}</p>
+            <p class="highlight my-1 px-3 py-1 rounded text-light">Costo di consegna € {{ restaurant.delivery_cost }}</p>
+            <p class="highlight my-1 px-3 py-1 rounded text-light">Spesa minima € {{ restaurant.min_purchase }}</p>
+        </div>
+    </div>
+    <!-- /restaurant details -->
     <!-- Restaurant menu -->
     <div class="container" v-if="restaurant">
-        <!-- Restaurant name -->
-        <h1 class="mt-5 mb-3">{{ restaurant.name }}</h1>
         <div class="row">
             <div class="col-9 row gy-2">
                 <!-- Recall ProductCard component and cycle it for every product -->
                 <div v-if="products" v-for="product in products" class="col-sm-12 col-md-6 col-lg-4">
-                    <ProductCard :product="product"/>
+                    <ProductCard :product="product" />
                 </div>
             </div>
             <div class="col-3 my-5">
-                <ProductsCart/>
+                <ProductsCart />
             </div>
         </div>
     </div>
@@ -53,16 +68,36 @@ export default {
 </template>
 
 <style lang="scss">
-    .ms-container {
-        max-width: 90rem;
-        // margin: 0 auto;
+@use '../assets/scss/_partials/variables' as *;
 
-        .ms-row {
+.ms-container {
+    max-width: 90rem;
+    // margin: 0 auto;
+
+    .ms-row {
+        // height: 100%;
+
+        .ms-col {
             // height: 100%;
-
-            .ms-col {
-                // height: 100%;
-            }
         }
     }
+}
+
+.restaurant-image {
+    max-height: 21.875rem;
+    overflow-y: hidden;
+}
+
+#restaurant-details {
+    border: 3px solid #f0f3f4;
+    border-radius: 20px;
+    position: relative;
+    top: -7.5rem;
+    margin: auto;
+    background-color: $ms_secondary_color_light;
+
+    .highlight {
+        background-color: $ms_secondary_color;
+    }
+}
 </style>
