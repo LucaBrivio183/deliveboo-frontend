6406ac7dca6c99e91d8e2aff12ade9abdeab460d
vittoriaromano <vittoria5005@gmail.com>
2023-06-26 20:47:02 +0400

    feat: Cart static graphic

--- src/components/ProductsCart.vue
+++ src/components/ProductsCart.vue
@@ -5,8 +5,26 @@ export default {
 </script>
 
 <template>
-    <div class="bg-secondary p-4 text-center">
-        <div><i class="fa-solid fa-cart-shopping fs-1"></i></div>
-        <h1 class="text-center bg-secondary">ciao</h1>
+    <div class="bg-secondary vh-100 overflow-y-scroll">
+        <div class="bg-success p-4 text-center ">
+            <div><i class="fa-solid fa-cart-shopping fs-1"></i></div>
+            <h1 class="text-center">Il tuo Carrello</h1>
+        </div>
+        <!-- agiungere i v-for="(product, index) in store.indexes"> -->
+        <div>
+            <div class="bg-light p-3 m-2">
+                Item 1
+            </div>
+        </div>
+        <div class="bg-light p-3 m-2">
+            Consegna:
+        </div>
+        <div class="bg-light p-3 m-2">
+            Totale:
+        </div>
+
+        <div class="text-center">
+            <a href="#" class="btn btn-success">Ordina!</a>
+        </div>
     </div>
 </template>
