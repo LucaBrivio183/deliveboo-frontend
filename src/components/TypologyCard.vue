05e5939efa12dbb5a5e199aaf060f7a83fe55486
LucaBrivio183 <l.brivio4@gmail.com>
2023-06-26 15:28:21 +0200

    fix: graphics

--- src/components/TypologyCard.vue
+++ src/components/TypologyCard.vue
@@ -7,7 +7,7 @@ export default {
     data() {
         return {
             store,
-            typologies: [],
+            isActive: false,
         }
     },
     props: {
@@ -17,12 +17,14 @@ export default {
 </script>
 
 <template>
-    <div id="typology-card" class="form-check d-flex flex-column h-100">
-        <input class="form-check-input d-none" type="checkbox" :value="typology.id" :id="typology.name"
-            v-model="store.selectedTypologies">
-        <label class="form-check-label" :for="typology.name" :name="typology.name">
-            <img class="img-fluid" src="https://picsum.photos/100/50" :alt="typology.name">
-        <small>{{ typology.name }}</small>
+    <div class="d-flex justify-content-center h-100 rounded" :class="{ 'clicked': this.isActive }" id="typology-card">
+        <!-- invisible checkbox for faster response on research -->
+        <input class="d-none" type="checkbox" :value="typology.id" :id="typology.name"
+            v-model="store.selectedTypologies" @change="this.isActive = !this.isActive">
+        <!-- label shown for make checkbox clickable like card -->
+        <label class="d-flex flex-column align-items-center p-2" :for="typology.name" :name="typology.name">
+            <img class="img-fluid rounded" src="https://picsum.photos/100/50" :alt="typology.name">
+            <small>{{ typology.name }}</small>
         </label>
     </div>
 </template>
@@ -32,13 +34,34 @@ export default {
 @use '../assets/scss/_partials/variables' as *;
 
 div{
+    border: 2px solid rgba($color: white, $alpha: 0);
+    &:hover{
+        border: 2px solid $ms_primary_color;
+    }
     img{
         aspect-ratio: 3/2;
         object-fit: contain;
         display: block;
     }
+    label {
+        cursor: pointer;
+        img{
+            object-fit: cover;
+            display: block;
+        }
+        small {
+            display: block;
+            font-size: .75rem;
+        }
+    }
+}
+.clicked{
+    border: 2px solid $ms_secondary_color !important; // background and dark text
+}
+
+@media screen and (width <= 576px) {
     small {
-        display: block;
+        font-size: .875rem;
     }
 }
 </style>
