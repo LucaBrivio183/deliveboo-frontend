de5687d676a056bcbcfbec2d2f5e67f789b10ed9
eugeniarossi <eugenia.rossi@outlook.com>
2023-06-27 10:41:08 +0200

    fix: products card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -89,23 +89,14 @@ export default {
 
 <template>
     <!-- Product card with modal trigger // productQuantity gets a reset after each card click -->
-    <div id="product-card" class="row mb-3 bg-light p-2 align-items-center text-dark">
+    <div id="product-card" class="row mb-3 bg-light p-2 align-items-center text-dark" data-bs-toggle="modal"
+        :data-bs-target="'#product' + product.id" @click="resetProductQuantity">
         <!-- col details -->
         <div class="col py-3">
-            <h3 class="mb-3">{{ product.name }}</h3>
-            <div class="d-flex justify-content-between">
-                <div>
-                    <span class="fw-bold">€ {{ product.price }}</span>
-                    <span class="fw-bold ms-3">{{ product.discount }}</span>
-                    <p class="badge rounded-pill py-2 px-3 text-light" data-bs-toggle="modal-ingredients" :data-bs-target="'#product' + product.id">Dettagli</p>
-                </div>
-                <div class="actions">
-                    <div class="btn" data-bs-toggle="modal" :data-bs-target="'#product' + product.id" @click="resetProductQuantity">+</div>
-                </div>
-                <!--<p>{{ product.ingredients 
-                    v-if="(product.discount !== 0)"
-                    }}</p>-->
-            </div>
+            <h5 class="mb-3 fw-bold">{{ product.name }}</h5>
+            <p>{{ product.ingredients }}</p>
+            <span :class="{ 'text-decoration-line-through': product.discount !== 0 }" class="fw-bold">€ {{ product.price }}</span>
+            <span v-if="(product.discount !== 0)" class="fw-bold ms-3">€ {{ product.discount }}</span>
         </div>
         <!-- /col details -->
         <!-- col image -->
@@ -126,23 +117,26 @@ export default {
                 <div class="modal-body">
                     <img src="https://picsum.photos/300/200" class="card-img-top h-50" :alt="product.name">
                     <div class="card-text mt-2" v-if="product.description">{{ product.description }}</div>
-                    <div class="card-text mt-2" v-if="product.ingredients"><strong>Ingredienti:</strong> {{ product.ingredients }}</div>
+                    <div class="card-text mt-2" v-if="product.ingredients"><strong>Ingredienti:</strong> {{
+                        product.ingredients }}</div>
                 </div>
                 <div class="modal-footer">
                     <div class="quantity w-100 text-center fs-5">
                         <!-- If productQuantity === 1, minus button is gray and not clickable -->
-                        <span :role="(this.productQuantity !== 1) ? 'button' : ''" @click="decreaseProductQuantity()"><i :class="`fa-solid fa-circle-minus  ${(this.productQuantity === 1) ? 'opacity-25' : ''}`"></i></span>
+                        <span :role="(this.productQuantity !== 1) ? 'button' : ''" @click="decreaseProductQuantity()"><i
+                                :class="`fa-solid fa-circle-minus  ${(this.productQuantity === 1) ? 'opacity-25' : ''}`"></i></span>
                         <span class="mx-3">{{ this.productQuantity }}</span>
-                        <span role="button" @click="increaseProductQuantity()"><i class="fa-solid fa-circle-plus"></i></span>
+                        <span role="button" @click="increaseProductQuantity()"><i
+                                class="fa-solid fa-circle-plus"></i></span>
                     </div>
                     <!-- The final product price gets changed and formatted to two decimals -->
-                    <button type="button" class="btn btn-primary w-100" @click="addItem">Aggiungi per {{ changeProductPrice.toFixed(2) }} €</button>
+                    <button type="button" class="btn btn-primary w-100" @click="addItem">Aggiungi per {{
+                        changeProductPrice.toFixed(2) }} €</button>
                 </div>
             </div>
         </div>
     </div>
     <!-- /Modal -->
-    
 </template>
 
 <style lang="scss">
